054666ce9c75321b5a86b746caff32dceeb2f6e3
santosh <santosh.ece06@gmail.com>
2018-11-17 20:56:43 +0530

    git remote set-url origin https://github.com/dacexam/180840320007.git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { isNgTemplate } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'facebook';
+  title = 'labexam';
+  
 }
---
+++ src/app/comp1/comp1.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { isNgTemplate } from '@angular/compiler';
+
+@Component({
+  selector: 'app-comp1',
+  templateUrl: './comp1.component.html',
+  styleUrls: ['./comp1.component.css']
+})
+export class Comp1Component  {
+
+  post='';
+  postList=[];
+
+  posthere()
+  {
+     const postObject={};
+     postObject['like']=0;
+     postObject['dislike']=0;
+     postObject['post']=this.post;
+     this.postList.splice(0,0,postObject);
+     this.post='';
+
+  }
+  likeCount(item)
+     {
+        item.like +=1;
+
+     }
+
+     dislikeCount(item)
+     {
+      item.dislike +=1;
+      
+     }
+  
+
+
+
+
+  
+  
+
+}
