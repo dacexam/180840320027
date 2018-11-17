dc0010c01d4a9b21b8ed5daf026539aa9d23cf5b
santosh <santosh.ece06@gmail.com>
2018-11-17 20:30:34 +0530

    git remote set-url origin https://github.com/dacexam/180840320020.git

--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -5,63 +5,106 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './comp1.component.html',
   styleUrls: ['./comp1.component.css']
 })
-export class Comp1Component  {
+export class Comp1Component implements OnInit {
 
-  post='';
-  postList=[];
-  update1='';
-  com='';
-  com1='';
-  posthere()
-  {
-    const obj={
-      comm:[],
-      'like':0,
-      'dislike':0,
-      'post':this.post,
-      'com':this.com,
-      'com1':this.com1
-    };
-if(this.post!=''){
-    this.postList.splice(0,0,obj);
-    this.post='';
-}
+  constructor() { }
+
+  ngOnInit() {
   }
 
-  like(ref){
-    ref.like+=1;
+  title = "DACBOOK";
+  postRef = '';
+  postList = [];
+
+  postMsg() {
+    if (this.postRef.trim().length == 0) {
+      alert("Post cannnot be empty!");
+    } else {
+      const post = {};
+      post['data'] = this.postRef;
+      post['likes'] = 0;
+      post['dislikes'] = 0;
+
+      post['editflag'] = false;
+      post['editPostRef'] = this.postRef;
+
+      post['commentcount'] = 0;
+      post['commentList'] = [];
+      post['commentRef'] = '';
+
+      this.postList.splice(0, 0, post);
+    }
+    this.postRef = '';
   }
 
-  dislike(ref){
-    ref.dislike+=1;
+  like(obj) {
+    obj['likes'] += 1;
   }
 
-delete1(ref){
-  let index=this.postList.indexOf(ref);
-  this.postList.splice(index,1);
-}
+  dislike(obj) {
+    obj['dislikes'] += 1;
+  }
 
-  update(ref){
-    if(ref.update1!=''){
-    ref.post=ref.update1;
-    ref.update1='';
+  edit(obj) {
+    obj['editflag'] = true;
+  }
+
+  saveMsg(obj) {
+    if (obj['editPostRef'].trim().length == 0) {
+      alert("Post cannnot be empty!");
+    } else {
+      obj['data'] = obj['editPostRef'];
+      obj['editflag'] = false;
     }
   }
 
-  comment(ref)
-  {
-    ref.comm.splice(0,0,this.com);
-    this.com='';
-  }
-  commdel(ref,value){
-    let index=ref.comm.indexOf(value);
-    ref.comm.splice(index,1);
-
+  canceleditMsg(obj) {
+    obj['editPostRef'] = obj['data'];
+    obj['editflag'] = false;
   }
 
-  commup(ref,value){
-    
-    ref.comm.value=ref.comm.com1;
-    ref.comm.com1='';
+  delete(obj) {
+    let i = this.postList.indexOf(obj);
+    this.postList.splice(i, 1);
+  }
+
+  addcomment(obj) {
+    if (obj.commentRef.trim().length == 0) {
+      alert("Comment cannnot be empty!");
+    } else {
+      const comment = {
+        data: obj.commentRef,
+        editdata: obj.commentRef,
+        editflag: false
+      }
+
+      obj['commentList'].push(comment);
+      obj['commentcount'] += 1;
+      obj['commentRef'] = '';
+    }
+  }
+
+  editcomment(obj) {
+    obj.editflag = true;
+  }
+
+  saveComment(obj) {
+    if (obj.editdata.trim().length == 0) {
+      alert("Comment cannnot be empty!");
+    } else {
+      obj['data'] = obj['editdata'];
+      obj.editflag = false;
+    }
+  }
+
+  canceleditComment(obj) {
+    obj['editdata'] = obj['data'];
+    obj.editflag = false;
+  }
+
+  deletecomment(obj1, obj2) {
+    let i = obj2.commentList.indexOf(obj1);
+    obj2.commentList.splice(i, 1);
+    obj2['commentcount'] -= 1;
   }
 }
