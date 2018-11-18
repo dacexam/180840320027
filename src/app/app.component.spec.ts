996b39b46a38098549c557696dbd0f11910f539f
santosh <santosh.ece06@gmail.com>
2018-11-18 08:11:08 +0530

    git remote set-url origin https://github.com/dacexam/180840320034.git

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular'`, () => {
+  it(`should have as title 'dac'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular');
+    expect(app.title).toEqual('dac');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to dac!');
   });
 });
--- src/app/comp1/comp1.component.ts
+++ src/app/comp1/comp1.component.ts
@@ -1,53 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-comp1',
   templateUrl: './comp1.component.html',
   styleUrls: ['./comp1.component.css']
 })
-export class Comp1Component {
+export class Comp1Component{
 
-  title = 'Angular';
-  post = '';
-  postList = [];
-  comm = '';
-  comment = [];
-  editedPost = '';
+  title='DAC';
+  post='';
+  postList=[];
 
   postHere(){
-    const postObject = {};
-    postObject['like'] = 0;
-    postObject['dislike'] = 0;
-    postObject['post'] = this.post;
-    postObject['comment'] = this.comment;
+    const postObject={};
+    postObject['like']=0;
+    postObject['dislike']=0;
+    postObject['post']=this.post;
 
-    this.postList.splice(0, 0, postObject);
-    this.post = '';
+    
+    postObject['comments']='';
+    postObject['carray']=[];
+    postObject['comment']=0;
+
+    this.postList.splice(0,0,postObject);
+    this.post='';
   }
 
   likeCount(item){
-    item.like += 1;
+      item.like+=1;
   }
 
   dislikeCount(item){
-    item.dislike += 1; 
+    item.dislike+=1;
   }
 
-  editPost(item){
-    item.post = this.editedPost;
-    this.editedPost = '';
+  deletePost(item){
+    this.postList.splice(this.postList.indexOf(item),1);
   }
 
-  deleteP(item){
-    let index = this.postList.indexOf(item);
-    this.postList.splice(index,1);
+  comment(item)
+  {
+    let c=item.comments;
+    item.carray.splice(0,0,c);
+    item.comment+=1;
+    item.comments='';
   }
 
-  commentHere(cont){
-    const com1 = {};
-    com1['comm'] = this.comm;
-    this.comment.splice(0,0,com1);
-    this.comm = '';
+  deleteComment(item)
+  {
+    item.carray.splice(item.carray.indexOf(item.carray),1);
+    item.comment-=1;
   }
+
 
 }
