cf8390f46a31d45332e7fc8f1665dc1ea69306ee
santosh <santosh.ece06@gmail.com>
2018-11-18 07:16:50 +0530

    git remote set-url origin https://github.com/dacexam/180840320065.git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cdac';
+  title = 'Dacbook';
+  post='';
+  edit='';
+  comment='';
+  editcom='';
+  postlist=[];
+
+  posthere(){
+    let obj={'like':0,
+    'dislike':0,
+    'post':this.post,
+    'commentlist':[]
+    };
+    if(this.post==''){
+      alert("Write at least something....");
+    }
+    else
+    {
+      this.postlist.splice(0,0,obj);
+      this.post='';
+    }
+  }
+
+  likecount(ref){
+    ref.like+=1;
+  }
+
+  dislikecount(ref){
+    ref.dislike+=1;
+  }
+
+  deletepost(ref){
+    this.postlist.splice(this.postlist.indexOf(ref),1);
+  }
+
+  editpost(ref){
+    if(this.edit==''){
+      alert("Enter something to edit your post");
+    }
+    else{
+      ref.post=this.edit;
+      this.edit='';
+    }
+  }
+
+  commenthere(ref){
+    if(this.comment=='')
+    {
+      alert("Enter a comment");
+    }
+    else{
+      ref.commentlist.splice(0,0,this.comment);
+      this.comment='';
+    }
+  }
+
+  deletecomment(ref,value){
+    let index=ref.commentlist.indexOf(value);
+    ref.commentlist.splice(index,1);
+  }
+
+  editcomment(ref,val){
+    if(this.editcom=='')
+    {
+      alert("Enter something to edit comment");
+    }
+    else
+    {
+    let index=ref.commentlist.indexOf(val);
+    ref.commentlist[index]=this.editcom;
+    this.editcom='';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/Forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { Comp1Component } from './comp1/comp1.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Comp1Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
